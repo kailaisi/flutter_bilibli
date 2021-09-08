f399ae161828caf94eafadc2ca9377462f4b6a23
kailaisii <541018378@qq.com>
2021-09-08 23:01:27 +0800

    hicache

---
+++ lib/db/hi_cache.dart
@@ -0,0 +1,54 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class HiCache {
+  SharedPreferences? _preferences;
+  HiCache._() {
+    init();
+  }
+
+  static HiCache? _instance;
+  static HiCache getInstance() {
+    if (_instance == null) {
+      _instance = HiCache._();
+    }
+    return _instance!;
+  }
+
+  void init() async {
+    if (_preferences == null) {
+      _preferences = await SharedPreferences.getInstance();
+    }
+  }
+
+  static Future<HiCache> preInit() async {
+    if (_instance == null) {
+      var perfs = await SharedPreferences.getInstance();
+      _instance = HiCache._pre(perfs);
+    }
+    return _instance!;
+  }
+
+  HiCache._pre(SharedPreferences perfs) {
+    _preferences = perfs;
+  }
+
+  setString(String key, String value) {
+    _preferences?.setString(key, value);
+  }
+
+  setDouble(String key, double value) {
+    _preferences?.setDouble(key, value);
+  }
+
+  setInt(String key, int value) {
+    _preferences?.setInt(key, value);
+  }
+
+  setBool(String key, bool value) {
+    _preferences?.setBool(key, value);
+  }
+
+  setStringList(String key, List<String> value) {
+    _preferences?.setStringList(key, value);
+  }
+}
